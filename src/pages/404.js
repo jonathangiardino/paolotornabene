9e26c4790971bfe43eac1e38e9b1bfbcb4e7b581
Jonathan <j.giardino@me.com>
2020-11-29 10:13:55 +0100

    Style 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,37 @@
 import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const PageContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-transform: uppercase;
+  h1 {
+    color: ${props => props.theme.colors.yellow};
+    font-size: 3rem;
+  }
+  p {
+    padding: 0 16px;
+  }
+  @media ${props => props.theme.breakpoints.medium} {
+    flex-direction: row;
+  }
+`
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <PageContainer>
+      <h1>404 |</h1>
+      <p> This page does not exist</p>
+      <Link to="/">Back Home</Link>
+    </PageContainer>
   </Layout>
 )
 
