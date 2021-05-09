6e2cc4d5842f0a19ce0fb6cda907df3e3e282172
Jonathan <j.giardino@me.com>
2021-05-09 13:28:05 +0200

    fix: remove controls for safari

--- src/components/backgroundVideo.js
+++ src/components/backgroundVideo.js
@@ -27,6 +27,26 @@ const VideoContainer = styled(motion.div)`
 const VideoSource = styled.video`
   width: 100%;
   pointer-events: none;
+  &::-webkit-media-controls-panel {
+    display: none !important;
+    -webkit-appearance: none;
+  }
+
+  /* Old shadow dom for play button */
+
+  &::-webkit-media-controls-play-button {
+    display: none !important;
+    -webkit-appearance: none;
+  }
+
+  /* New shadow dom for play button */
+
+  /* This one works! */
+
+  &::-webkit-media-controls-start-playback-button {
+    display: none !important;
+    -webkit-appearance: none;
+  }
 `
 
 const BackgroundVideo = ({ source }) => {
